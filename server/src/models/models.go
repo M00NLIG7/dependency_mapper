e04c47d9b4b022ea58dab89f2fe9a8d6566b57de
Hunter-Pittman <hunterpittman@protonmail.com>
2024-04-07 12:07:45 -0700

    Revised depmap server inital commit

---
+++ server/src/models/models.go
@@ -0,0 +1,27 @@
+package models
+
+// need to add constraints
+
+// Maybe add CPE string for ndoe existing service comparison?
+
+// Database model
+type Node struct {
+	ID          uint `gorm:"primaryKey"`
+	NodeType    string
+	Module      string
+	DestPort    int
+	DestIp      string
+	SrcPort     int
+	SrcIp       string
+	Description string
+}
+
+// Database model
+type Edge struct {
+	ID           uint `gorm:"primaryKey"`
+	SrcNodeID    uint // Foreign key for the source Node
+	DestNodeID   uint // Foreign key for the destination Node
+	Relationship string
+	SrcNode      Node `gorm:"foreignKey:SrcNodeID"`  // Specifies SrcNode as the foreign key relationship
+	DestNode     Node `gorm:"foreignKey:DestNodeID"` // Specifies DestNode as the foreign key relationship
+}
