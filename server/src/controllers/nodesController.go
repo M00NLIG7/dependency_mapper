cfda74c63c614b77f0ac97838e579993386416fe
Hunter-Pittman <hunterpittman@protonmail.com>
2024-04-12 13:19:02 -0700

    Added adjacency matrix and debugging statements

--- server/src/controllers/nodesController.go
+++ server/src/controllers/nodesController.go
@@ -1,26 +1,90 @@
 package controllers
 
 import (
+	"crypto/sha256"
 	"depedency-mapper-server/initializers"
 	"depedency-mapper-server/models"
-	"fmt"
+	"encoding/hex"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
 
-func edgeCheck() {
-	fmt.Println("Yeah we checkin edges bruh")
+var AdjacencyMatrix map[string][]string
+
+func EdgeCheck() {
+	var allNodes []models.Node
+	uniqueNodesResult := initializers.DB.Find(&allNodes)
+	zap.S().Infof("There are %v records: %v", uniqueNodesResult.RowsAffected, allNodes)
+
+	if uniqueNodesResult.Error != nil {
+		zap.S().Error("Error: ", uniqueNodesResult.Error)
+		return
+	}
+
+	for _, node := range allNodes {
+		if AdjacencyMatrix == nil {
+			// intialize map
+			zap.S().Info("Initializing map...")
+			AdjacencyMatrix = make(map[string][]string)
+		}
+
+		// check if key exists
+		_, ok := AdjacencyMatrix[node.LocalIp]
+		if !ok {
+			zap.S().Infof("%s does not exist", node.LocalIp)
+			AdjacencyMatrix[node.LocalIp] = []string{}
+		}
+		// Check if the destination IP already exists in the specifed key value
+		counter := 0
+		if node.RemoteIp != "0.0.0.0" {
+			for _, RemoteIp := range AdjacencyMatrix[node.LocalIp] {
+				if RemoteIp == node.RemoteIp {
+					zap.S().Infof("%s destination ID already loaded", RemoteIp)
+					counter++
+				}
+			}
+		} else {
+			counter++
+		}
+
+		if counter == 0 {
+			// Add dest ip to EdgeMap slice
+			AdjacencyMatrix[node.LocalIp] = append(AdjacencyMatrix[node.LocalIp], node.RemoteIp)
+		}
+	}
+}
+
+func EdgeCheckEndpoint(c *gin.Context) {
+	EdgeCheck()
+	zap.S().Infof("Current edge structure: %v", AdjacencyMatrix)
+}
+
+func generateNodeSignature(node models.Node) string {
+	data := node.LocalIp + strconv.Itoa(node.LocalPort) + node.RemoteIp + strconv.Itoa(node.RemotePort) + node.Module + node.NodeType
+	h := sha256.New()
+	h.Write([]byte(data))
+	hashBytes := h.Sum(nil)
+
+	// Convert hash bytes to hex string
+	hashString := hex.EncodeToString(hashBytes)
+	return hashString
 }
 
 func AddNode(c *gin.Context) {
-	var node models.Node
-	if err := c.BindJSON(&node); err != nil {
+	var newNode models.Node
+	if err := c.BindJSON(&newNode); err != nil {
 		zap.S().Info("Error binding JSON data")
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
+	// Create signature for new node
+	newNodeSig := generateNodeSignature(newNode)
+	// Assign new signature to the new node struct
+	newNode.Signature = newNodeSig
 
 	var allNodes []models.Node
 	nodesResult := initializers.DB.Find(&allNodes)
@@ -31,24 +95,15 @@ func AddNode(c *gin.Context) {
 		return
 	}
 
-	for _, savedNode := range allNodes {
-		// Save DB node to internal node type
-		transformedNode := models.Node{
-			NodeType:    savedNode.NodeType,
-			Module:      savedNode.Module,
-			DestPort:    savedNode.DestPort,
-			DestIp:      savedNode.DestIp,
-			SrcPort:     savedNode.SrcPort,
-			SrcIp:       savedNode.SrcIp,
-			Description: savedNode.Description,
-		}
-		if transformedNode == node {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Node already exists!"})
+	for _, dbNode := range allNodes {
+		if dbNode.Signature == newNodeSig {
+			zap.S().Error("Node already exists!")
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Node already exists"})
 			return
 		}
 	}
 
-	result := initializers.DB.Create(&node)
+	result := initializers.DB.Create(&newNode)
 
 	if result.Error != nil {
 		zap.S().Info("Error adding new data to DB")
@@ -56,19 +111,10 @@ func AddNode(c *gin.Context) {
 		return
 	}
 
+	EdgeCheck()
+
 	// List some debugging stats
 	zap.S().Infof("Rows affected: %v", result.RowsAffected)
+	zap.S().Infof("Current edge structure: %v", AdjacencyMatrix)
 	c.JSON(http.StatusOK, gin.H{"message": "DB updated!"})
 }
-
-// Test data
-
-// node := models.Node{
-// 	NodeType:    "service",
-// 	Module:      "core",
-// 	DestPort:    3306,
-// 	DestIp:      "192.168.60.33",
-// 	SrcPort:     60888,
-// 	SrcIp:       "192.168.60.22",
-// 	Description: "This is test data!",
-// }
